a89ca6fa18fdb4009ef24845080bd26e81c81b88
bilel <bilel@disbrulix.home>
2021-02-25 21:17:29 +0100

    Restructuration architecture du projet et ajout des fichiers sources.

---
+++ src/cheater_transaction.c
@@ -0,0 +1,12 @@
+/**
+ * @file block.c
+ * @author Bilel, Cedric
+ * @brief 
+ * @version 0.1
+ * @date 2021-02-25
+ * 
+ * @copyright Domaine public 2021
+ * 
+ */
+
+#include "../include/cheater_transaction.h"
