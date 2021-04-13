052f900e831a410731815357cacc0b342cfe8f50
bkun31 <bilel@mxArch.localdomain>
2021-04-13 15:01:20 +0200

    Ajout jeux de tests Block et d'un script de test

---
+++ test/block-tests/test_block.c
@@ -0,0 +1,131 @@
+/**
+ * @file test_block.c
+ * @author Cédric Carro (cedric.carro@univ-tlse3.fr)
+ * @author Asma Hamza (asma.hamza@univ-tlse3.fr)
+ * @author Bilel Besseghieur (bilel.besseghieur@univ-tlse3.fr)
+ * @brief Tests implémentation TAD Block
+ * @version 0.1
+ * @date 2021-04-12
+ * 
+ * @copyright Domaine public 2021
+ * 
+ */
+
+#include <stdlib.h>
+#include <string.h>
+#include "../../include/block.h"
+
+#define OK 0
+#define UNABLE_OPEN_FILE 1
+
+#define TRANSACTION_PATH "transaction.txt"
+#define HASH_PATH "hash.txt"
+
+#define HASH_SIZE SHA256_BLOCK_SIZE * 2 + 1
+#define TRANSACTION_SIZE 140
+
+#define DIFFICULTY 4
+
+/* returne une transaction depuis un fichier f */
+char *transaction(FILE *f)
+{
+    char tx_char[140];
+    char *tx_dynamic;
+    fgets(tx_char, TRANSACTION_SIZE, f);
+    tx_dynamic = malloc(sizeof(char) * (strlen(tx_char) + 1));
+    strcpy(tx_dynamic, tx_char);
+
+    return tx_dynamic;
+}
+
+/* libère la transaction crée plus haut */
+void free_tx(void *tx)
+{
+    free((char *)tx);
+}
+
+int main()
+{
+    FILE *f_tx;   /* on va piocher dans un fichier des transactions */
+    FILE *f_hash; /* idem on va piocher des hash pour construire le bloc */
+
+    f_tx = fopen(TRANSACTION_PATH, "r");
+
+    if (f_tx == NULL)
+    {
+        perror(TRANSACTION_PATH);
+        exit(UNABLE_OPEN_FILE);
+    }
+
+    f_hash = fopen("hash.txt", "r");
+
+    if (f_hash == NULL)
+    {
+        perror(HASH_PATH);
+        fclose(f_tx);
+        exit(UNABLE_OPEN_FILE);
+    }
+
+    else
+    {
+        SkipList sl_tx = skiplist_create(4);
+        Block blk;
+        char merkleT[SHA256_BLOCK_SIZE * 2 + 1];
+        char prevhash[SHA256_BLOCK_SIZE * 2 + 1];
+        int tx_count;
+
+        /* récupère le nombre de transactions qui doit être pioché, le fichier doit donc commencer par un entier indiquant ce nombre */
+        fscanf(f_tx, "%d", &tx_count);
+        getc(f_tx); /* pour récupérer le retour à la ligne qui n´a pas été récupéré avec fscanf sinon il y a un bug */
+
+        /* on construit la liste des transactions pour le bloc */
+        while (tx_count)
+        {
+            skiplist_insert(sl_tx, tx_count--, transaction(f_tx));
+        }
+
+        /* on va créer le bloc on récupère deux hash pour simuler le merkle tree et le hash du bloc précédent */
+        fscanf(f_hash, "%s", merkleT);
+        fscanf(f_hash, "%s", prevhash);
+
+        printf("Creation bloc et affichage des informations du bloc  avant minage, avec les getters.\n\n");
+        blk = block_create(2, sl_tx, merkleT, prevhash);
+        printf("index : %d\n" /* on regarde que chaque getters fonctionne et que les informations sont cohérentes (bloc bien construit) */
+               "nonce : %d\n"
+               "hash : %s\n"
+               "hashprev : %s\n"
+               "merkleTree :%s\n"
+               "timestamp : %ld\n"
+               "tx_count : %d\n",
+               block_index(blk),
+               block_nonce(blk),
+               block_hash(blk),
+               block_prevhash(blk),
+               block_merkle_tree(blk),
+               block_timestamp(blk),
+               block_tx_count(blk));
+
+        printf("\nMinage du bloc !\n");
+        proof_of_work(blk, DIFFICULTY); /* on mine le bloc */
+
+        /* j´ajoute les hash dans les fichiers textes pour pouvoir les réutiliser si jamais,
+        à décommenter si besoin d´ajouter des hash au fichier test */
+        // fseek(f_hash, 0, SEEK_END);
+        // fprintf(f_hash, "\n%s", block_hash(blk));
+
+        printf("\nAffichage informations du bloc après minage, avec block_dump.\n");
+        block_dump(stdout, blk); /* on vérifie que la fonction dump fonctionne */
+
+        /* on vérifie la longueur du hash du bloc miné et qu´il commence bien par le nombre de 0 voulu */
+        int long_hash = strlen(block_hash(blk)); // sa longueur en caractères hexadécimaux
+        printf("Le sha : %s avec %d caractères hexadécimaux, soit %d octets comme prévu\n", block_hash(blk), long_hash, long_hash / 2);
+
+        /* on n´oublie pas de tout libérer ! */
+        fclose(f_tx);
+        fclose(f_hash);
+        skiplist_delete(sl_tx, free_tx);
+        free(blk);
+    }
+
+    return OK;
+}
---
+++ test/test.sh
@@ -0,0 +1,117 @@
+#!/bin/zsh
+
+#run unit tests (all by default)
+#Usage : $0 [test_num]
+#test_num value :
+# 1 test block
+# 2 test blockchain
+# 3 test transaction
+# 4 test user
+# 5 test bnb
+# 6 test cheater block
+# 7 test cheater transaction
+
+cd $(dirname $0)
+
+if [ $# -ge 2 ]; then
+    echo "Usage : $0 [test_num]" >&2
+    exit 1
+fi
+
+if [ $# -eq 0 ]; then
+    gcc -Wall -Werror -std=c99 ../src/block.c ../src/skiplist.c -g ../src/rng.c ../src/sha256_utils.c ../src/sha256.c block-tests/test_block.c -o block-tests/test_block
+    gcc -Wall -Werror -std=c99 ../src/blockchain.c blockchain-tests/test_blockchain.c -o blockchain-tests/test_blockchain
+    gcc -Wall -Werror -std=c99 ../src/transaction.c transaction-tests/test_transaction.c -o transaction-tests/test_transaction
+    gcc -Wall -Werror -std=c99 ../src/user.c user-tests/test_user.c -o user-tests/test_user
+    gcc -Wall -Werror -std=c99 ../src/bnb.c bnb-tests/test_bnb.c -o bnb-tests/test_bnb
+    gcc -Wall -Werror -std=c99 ../src/cheater_block.c cheater_block-tests/test_cheater_block.c -o cheater_block-tests/test_cheater_block
+    gcc -Wall -Werror -std=c99 ../src/cheater_transaction.c cheater_transaction-tests/test_cheater_transaction.c -o cheater_transaction-tests/test_cheater_transaction
+
+    cd block_tests/
+    echo "Run test_block"
+    block-tests/test_block
+
+    cd ../blockchain-tests/
+    echo "Run test_blockchain"
+    blockchain-test/test_blockchain
+
+    cd ../transaction-tests/
+    echo "Run test_transaction"
+    transaction-tests/test_transaction
+
+    cd ../user-tests/
+    echo "Run test_user"
+    user-tests/test_user
+
+    cd ../bnb-tests/
+    echo "Run test_bnb"
+    bnb-tests/test_bnb
+
+    cd ../cheater_block-tests/
+    echo "Run test_cheater_block"
+    cheater_block-tests/test_cheater_block
+
+    cd ../cheater_transaction-tests/
+    echo "Run test_cheater_transaction"
+    cheater_transaction-tests/cheater_transaction
+
+else
+    if [ "1" -eq $1 ]; then
+        gcc -Wall -Werror -std=c99 ../src/block.c ../src/skiplist.c -g ../src/rng.c ../src/sha256_utils.c ../src/sha256.c block-tests/test_block.c -o block-tests/test_block
+        cd block-tests/
+
+        echo "Run test_block"
+        echo ""
+        ./test_block
+
+    elif [ "2" -eq $1 ]; then
+        gcc -Wall -Werror -std=c99 ../src/blockchain.c ../src/skiplist.c -g ../src/rng.c ../src/sha256_utils.c ../src/sha256.c blockchain-tests/test_blockchain.c -o blockchain-tests/test_blockchain
+        cd blockchain-tests/
+
+        echo "Run test_blockchain"
+        echo ""
+        ./test_blockchain
+
+    elif [ "3" -eq $1 ]; then
+        gcc -Wall -Werror -std=c99 ../src/transaction.c ../src/skiplist.c -g ../src/rng.c ../src/sha256_utils.c ../src/sha256.c transaction-tests/test_transaction.c -o transaction-tests/test_transaction
+        cd transaction-tests/
+
+        echo "Run test_transaction"
+        echo ""
+        ./test_transaction
+
+    elif [ "4" -eq $1 ]; then
+        gcc -Wall -Werror -std=c99 ../src/user.c ../src/bnb.c ../src/queue.c user-tests/test_user.c -o user-tests/test_user
+        cd user-tests/
+
+        echo "Run test_user"
+        echo ""
+        ./test_user
+
+    elif [ "5" -eq $1 ]; then
+        gcc -Wall -Werror -std=c99 ../src/bnb.c bnb-tests/test_bnb.c -o bnb-tests/test_bnb
+        cd bnb-tests/
+
+        echo "Run test_bnb"
+        echo ""
+        ./test_bnb
+
+    elif [ "6" -eq $1 ]; then
+        gcc -Wall -Werror -std=c99 ../src/cheater_block.c ../src/skiplist.c -g ../src/rng.c ../src/sha256_utils.c ../src/sha256.c cheater_block-tests/test_cheater_block.c -o cheater_block-tests/test_cheater_block
+        cd cheater_block-tests/
+
+        echo "Run test_cheater_block"
+        echo ""
+        ./test_cheater_block
+
+    elif [ "6" -eq $1 ]; then
+        gcc -Wall -Werror -std=c99 ../src/cheater_transaction.c ../src/skiplist.c -g ../src/rng.c ../src/sha256_utils.c ../src/sha256.c cheater_transaction-tests/test_cheater_transaction.c -o cheater_transaction-tests/test_cheater_transaction
+        cd cheater_transaction-tests/
+
+        echo "Run test_cheater_transaction"
+        echo ""
+        ./test_cheater_transaction
+    fi
+fi
+
+exit 0
