2d9eb06a36ca5d7272865d84192126ddf072f2ea
Soom26 <asmahamza523@gmail.com>
2021-04-18 01:03:57 +0200

    cheaters

--- include/cheater_transaction.h
+++ include/cheater_transaction.h
@@ -1,22 +1,42 @@
-/**
- * @file cheater_transaction.h
- * @author Bilel, Cedric
- * @brief 
- * @version 0.1
- * @date 2021-02-25
+#ifndef CHEATER_TRANSACTION_H #define CHEATER_TRANSACTION_H /* ------------ Déclaration Type : ------------ /
+
+typedef struct cheater;
+
+/ ------------ Déclaration Constructeur : ------------ /
+
+
+/ ------------ Déclaration Opérateur : ------------ */ 
+/*
+ * @brief parcourt la liste doublement chainée jusqu'à trouver la transaction et le bloc saisis 
+ * @param numTx numéro de la transaction à supprimer 
+ * @param *bc adresse de la blockchain où on va supprimer le bloc 
+ * @param index Numéro du bloc
+ * @return suppression du bloc 
+*/
+
+void cheaterSuppressionTx(int numTx, int index, blockchain *bc);
+
+ 
+/* @brief modifie le numero des transactions afin de ne laisser aucune trace *
+ 
+ * @param int index l'index qu'on va incrémenter de 1 à chaque fois à partir du dernier bloc 
+ * @return 
+ */ 
+int transactionIndex(int index);
+
+/*
+ * @brief recalcule les merkle root 
  * 
- * @copyright Domaine public 2021
- * 
- */
-#ifndef CHEATER_TRANSACTION_H
-#define CHEATER_TRANSACTION_H
+ * @param hash Hash du merkle root à partir de laquelle on modifiera les hashs suivants 
+ * @return 
+*/
 
-/* ------------ Déclaration Type : ------------ */
+char cheaterMerkleRootTx(char hash[SHA256_BLOCK_SIZE2+1]); 
 
+/* @brief recalcule les hash suivants
+  *
+  * @param hash Hash de la transaction à partir de laquelle on modifiera les hashs suivants
+  * @return
+*/
 
-/* ------------ Déclaration Constructeur : ------------ */
-
-
-/* ------------ Déclaration Opérateur : ------------ */
-
-#endif
+char cheaterRecalculTx(char hash[SHA256_BLOCK_SIZE2+1]); #endif
