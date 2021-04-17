2cc499aa9f45ed219b57e6b02ae3412df29e35b4
Soom26 <asmahamza523@gmail.com>
2021-04-18 00:03:02 +0200

    modification transactions

--- src/transaction.c
+++ src/transaction.c
@@ -20,26 +20,100 @@
 
 Transaction generate_transaction(User user_source, User user_destination)
 {
-    Transaction tx;
-    (void)user_source;
-    (void)user_destination;
-    return tx;
+	/*on doit préparer le format de la transaction qui est : 
+	 *"Source usern-Destination : userm montant_transaction"
+	 *on doit générer un nombre aléatoire qui sera le montant (inférieur à MAX_VALUE et au user.wallet)
+	 */
+	int MAX_VALUE=50;
+	int montantMaximal;
+	if MAX_VALUE < user_source.wallet //pour choisir un nombre qui dépasse pas le wallet ni le max value 
+	{
+		montantMaximal = MAX_VALUE; 
+	}
+	else 
+	{
+		montantMaximal = user_source.wallet; 
+	}
+	//fonction qui génère un nombre aléatoire 
+	srand(time(NULL)); 
+	int montantAleatoire=rand()%montantMaximal+1; //génère un nombre entre 1 et MAX_VALUE ou user_source.wallet
+	return("Source user %i -Destination : user %i %i", user_source, user_destination, montantAleatoire); 
 }
 
 SkipList construct_transaction_list(Queue *transactions_queue)
 {
-    SkipList d;
-    (void)transactions_queue;
-    return d;
+	/*On vérifie si MAX_TRANSACTIONS est inférieur au nombre d'éléments de la file (besoin de connaître la taille de la file)
+	 * (besoin d'accéder à la file pour récupérer les transacitons, normalement avec pop)
+	 * SI OUI : on pop un nombre aléatoire de transactions
+	 * (besoin de générer un nombre aléatoire)
+	 * SI NON : on prend toutes les transactions restantes
+	 */
+	 srand(time(NULL));
+	 if MAX_TRANSACTIONS < queueSize(transactions_queue)
+	 {
+		 int nbAleatoireTransactions=rand()%10+1; //génère un nombre aléatoire de transactions avec 10 la constante
+		 for (i=0; i<nbAleatoireTransactions; i++)
+		 {
+			 Transaction transaction=queuePop(q);
+			 insererDansFile(transaction); //fonction qui prend l'element reçu en parametre et l'insere dans la file (renvoie une file)TP2
+		 }
+     }
+     else 
+     {
+		 while (queueSize(transactions_queue) != 0) 
+		 {
+			 Transaction transaction=queuePop(q);
+			 insererDansListe(transaction);
+		 }
+	 }	 
+	 return transactions_list;
 }
 
-
-void calculate_merkleTree(SkipList transactions_list, char hash[SHA256_BLOCK_SIZE * 2 + 1])
+//fonction insererDansListe qui retourne transactions_list 
+Queue insererDansFile (Transaction transaction) 
 {
-    (void)transactions_list;
-    (void)hash;
-}
+	
+//fonction selectionElement
 
+char *calculate_merkleTree(Queue transactions_queue, char hash[SHA256_BLOCK_SIZE * 2 + 1])
+{
+	for (i=0, i<nbTransactions, i++)// ? ? ? ? 
+	{
+		 char hashRes[SHA256_BLOCK_SIZE*2 + 1];
+		 char * var = selectionElement(transactions_queue); //on sélectionne la transaction que l'on insére dans la variable
+		 sha256ofString(var ,hashRes); //on obtient le hash 
+		 char tab[nbTransactions]; //on cree un tableau pour le hash des transactions
+		 tab[i]=hashRes; //on ajoute le hash de la transaciton au tableau
+		 i++; //on passe à l'index suivant 
+	}
+	int j=0;//index du tableau où on insère les elements
+	int i=0;//index du tableau duquel on prend les éléments 
+	if (nbTransactions%2!=0) //si le nombre de transactions est impair 
+	{
+		tab[nbTransactions]=tab[nbTransactions-1] //on dédouble la case pour pouvoir calculer le hash 
+	}
+	while (i <nbTransactions)
+	{
+		char * concatenation //on crée une variable concatenation qui contient la concatenation
+		concatenation = strcat(tab[i], tab[i+1]); //on concatene 2 à 2 les elements du tableau 
+		char tableau[nbTransactions] 
+		tableau[j]=sha256ofString(concatenation); //on insere le hash de la concatenation dans notre nouveau tableau  
+		i+=2;
+		j+=1;
+
+	} 
+	while (tab[1]!=NULL) //maintenant on fait la boucle jusqu'à ce qu'on atteint le merkle root 
+	{
+		char * concatenation; //on crée une variable concatenation qui contient la concatenation
+		concatenation = strcat(tableau[i], tableau[i+1]); //on concatene 2 à 2 les elements du tableau 
+		tableau[j]=sha256ofString(concatenation); //on insere le hash de la concatenation dans notre nouveau tableau 
+		int tailleTableau = sizeof(tableau)/sizeof(tableau[0]);   //on diminue la taille du tableau 
+		tailleTableau--;
+		i+=2;
+		j+=1;
+		return tableau[0];
+	}
+}
 
 void print_transaction(FILE *f, Transaction tx)
 {
